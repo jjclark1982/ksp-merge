789e7811f5607bc509f5911a50d62c8b2981eeaf
Jesse Clark <jjclark1982@gmail.com>
2014-08-11 17:51:55 +1000

    mass-based vessel size

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm saves/merged*
+rm saves/merged* saves/persistent.sfs
 
 for F in saves/*.sfs; do
     ./sfs2json < $F > $F.json
@@ -9,6 +9,6 @@ done
 
 ./merge-saves saves/0.24.sfs.json saves/*derelict* > saves/merged.json
 
-./json2sfs < saves/merged.json > saves/merged.sfs
+./json2sfs < saves/merged.json > saves/persistent.sfs
 
 rm saves/*.json
